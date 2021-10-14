e517e8e690fe2d4684d41419ce55fd436b61e9e6
aronunlabeled <xaron97x@hotmail.com>
2021-10-14 14:17:45 -0400

    first

---
+++ PairPraactice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PairPraactice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
